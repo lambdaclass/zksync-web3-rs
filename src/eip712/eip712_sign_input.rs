7e3eb35a7785e8f59648d0c1337ea6ecfcd3aab5
IAvecilla <iavecilla@fi.uba.ar>
2023-05-23 18:28:58 -0300

    Mark struct hash function as todo

--- src/eip712/eip712_sign_input.rs
+++ src/eip712/eip712_sign_input.rs
@@ -3,7 +3,6 @@ use ethers::types::{
     Address, Bytes, U256,
 };
 use serde::{Deserialize, Serialize};
-use sha2::Digest;
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
@@ -22,7 +21,7 @@ pub struct Eip712SignInput {
     pub nonce: U256,
     pub value: Option<U256>,
     pub data: Option<Bytes>,
-    pub factory_deps: Option<Vec<Bytes>>,
+    pub factory_deps: Option<Vec<u8>>,
     pub paymaster_input: Option<Vec<u8>>,
 }
 
@@ -34,9 +33,8 @@ impl Eip712 for Eip712SignInput {
             name: Some(String::from("zkSync")),
             version: Some(String::from("2")),
             chain_id: Some(U256::from(9_i32)),
-            // TODO: Get the actual zkSync contract address.
             verifying_contract: "0xDAbb67b676F5b01FcC8997Cc8439846D0d8078ca".parse().ok(),
-            salt: None,
+            salt: Some([0_u8; 32]),
         })
     }
 
@@ -50,31 +48,6 @@ impl Eip712 for Eip712SignInput {
     /// * The second 2 bytes denote the length of the bytecode in 32-byte words.
     /// * The rest of the 28-byte (i.e. 28 low big-endian bytes) are equal to the last 28 bytes of the sha256 hash of the contract's bytecode.
     fn struct_hash(&self) -> Result<[u8; 32], Self::Error> {
-        let step_1: [u8; 2] = 0x100_u16.to_be_bytes();
-        let step_2: [u8; 2] = ((self
-            .factory_deps
-            .clone()
-            .ok_or_else(|| return Eip712Error::FailedToEncodeStruct)?[0]
-            .len()
-            / 32) as u16)
-            .to_be_bytes();
-        let step_3: [u8; 28] = sha2::Sha256::digest(
-            &self
-                .factory_deps
-                .clone()
-                .ok_or_else(|| return Eip712Error::FailedToEncodeStruct)?[0],
-        )
-        .into_iter()
-        .skip(4)
-        .collect::<Vec<u8>>()
-        .try_into()
-        .unwrap();
-
-        let mut contract_hash: [u8; 32] = [0; 32];
-        contract_hash[..2].clone_from_slice(&step_1);
-        contract_hash[2..4].clone_from_slice(&step_2);
-        contract_hash[4..].clone_from_slice(&step_3);
-
-        Ok(contract_hash)
+        todo!()
     }
 }
