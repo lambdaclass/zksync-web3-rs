a58028501677c5573510eef9c7c7752491f47590
Iván Litteri <ilitteri@fi.uba.ar>
2023-05-24 15:04:35 -0300

    Update `test_eip712`

--- src/eip712/eip712_sign_input.rs
+++ src/eip712/eip712_sign_input.rs
@@ -2,7 +2,8 @@ use ethers::{
     abi::encode,
     types::{
         transaction::eip712::{
-            encode_data, encode_type, EIP712Domain, Eip712, Eip712DomainType, Eip712Error, Types,
+            encode_data, encode_type, EIP712Domain, EIP712WithDomain, Eip712, Eip712DomainType,
+            Eip712Error, Types,
         },
         Address, Bytes, U256,
     },
@@ -124,16 +125,22 @@ impl Eip712 for Eip712SignInput {
     }
 
     fn type_hash() -> Result<[u8; 32], Self::Error> {
-        todo!()
+        Ok(keccak256(encode_type(
+            "zkSync",
+            &eip712_sign_input_types(),
+        )?))
     }
 
     fn struct_hash(&self) -> Result<[u8; 32], Self::Error> {
-        let types = eip712_sign_input_types();
-        let type_hash = keccak256(encode_type("zkSync", &types)?);
+        let type_hash = <EIP712WithDomain<Self> as Eip712>::type_hash()?;
         Ok(keccak256(
             [
                 &type_hash,
-                &encode(&encode_data("zkSync", &json!(self), &types)?)[..],
+                &encode(&encode_data(
+                    "zkSync",
+                    &json!(self),
+                    &eip712_sign_input_types(),
+                )?)[..],
             ]
             .concat(),
         ))
@@ -145,7 +152,7 @@ mod tests {
     use super::*;
     use crate::{
         eip712::{
-            eip712_transaction_request::Eip712Meta,
+            eip712_transaction_request::{Eip712Meta, PaymasterParams},
             utils::{DEFAULT_GAS_PER_PUBDATA_LIMIT, EIP712_TX_TYPE},
             Eip712TransactionRequest,
         },
@@ -153,36 +160,55 @@ mod tests {
         zks_utils::CONTRACT_DEPLOYER_ADDR,
     };
     use ethers::{
-        abi::encode,
+        prelude::{k256::ecdsa::SigningKey, MiddlewareBuilder},
         providers::{Middleware, Provider},
-        utils::keccak256,
+        signers::Signer,
+        signers::Wallet,
+        types::Signature,
+        utils::{
+            keccak256,
+            rlp::{Encodable, Rlp, RlpStream},
+        },
     };
 
     #[tokio::test]
     async fn test_eip712() {
+        /* Create Wallet */
+
+        let mut wallet = "0xf12e28c0eb1ef4ff90478f6805b68d63737b7f33abfa091601140805da450d93"
+            .parse::<Wallet<SigningKey>>()
+            .unwrap();
+
         /* Connect to node */
 
         let provider = Provider::try_from(format!(
             "http://{host}:{port}",
-            host = "65.108.204.116",
+            // host = "65.108.204.116",
+            host = "localhost",
             port = 3050
         ))
-        .unwrap();
+        .unwrap()
+        .with_signer(wallet.clone());
 
         /* Create Transaction */
 
         let mut tx = Eip712TransactionRequest::default();
 
         tx.r#type = EIP712_TX_TYPE.into();
-        tx.from = "0xbd29A1B981925B94eEc5c4F1125AF02a2Ec4d1cA".parse().ok();
+        tx.from = "0x8002cD98Cfb563492A6fB3E7C8243b7B9Ad4cc92".parse().ok();
         tx.to = CONTRACT_DEPLOYER_ADDR.parse().ok();
         tx.chain_id = 270.into();
 
-        // let fee = provider.estimate_fee(tx.clone()).await.unwrap();
+        let fee = provider.estimate_fee(tx.clone()).await.unwrap();
 
-        // tx.max_priority_fee_per_gas = Some(fee.max_priority_fee_per_gas);
-        // tx.max_fee_per_gas = Some(fee.max_fee_per_gas);
-        // tx.gas_limit = Some(fee.gas_limit);
+        tx.max_priority_fee_per_gas = Some(fee.max_priority_fee_per_gas);
+        tx.max_fee_per_gas = Some(fee.max_fee_per_gas);
+        tx.gas_limit = Some(fee.gas_limit);
+
+        // tx.max_priority_fee_per_gas = Some(U256::from(500000000));
+        // tx.max_fee_per_gas = Some(U256::from(50000));
+        // tx.gas_limit = Some(U256::zero());
+        tx.gas_price = Some(U256::one());
 
         // Build data
         let build_data = |function_signature: &str| -> eyre::Result<Vec<u8>> {
@@ -214,20 +240,61 @@ mod tests {
 
         tx_sign_input.gas_per_pubdata_byte_limit = Some(DEFAULT_GAS_PER_PUBDATA_LIMIT.into());
 
+        /* Update Wallet */
+
+        wallet = Wallet::with_chain_id(
+            wallet,
+            tx_sign_input.domain().unwrap().chain_id.unwrap().as_u64(),
+        );
+
         /* Testing */
 
-        let encoded_type = encode_type("zkSync", &eip712_sign_input_types()).unwrap();
-        let type_hash = keccak256(&encoded_type);
-        println!("{:?}", encoded_type);
-        println!("{:?}", type_hash);
+        if let Some(custom_data) = &mut tx.custom_data {
+            let signature: Signature = Wallet::sign_typed_data(&wallet, &tx_sign_input)
+                .await
+                .unwrap();
+            let signature_bytes = Bytes::from(signature.to_vec());
+            custom_data.custom_signature = Some(signature_bytes);
 
-        let encoded_data =
-            encode_data("zkSync", &json!(tx_sign_input), &eip712_sign_input_types()).unwrap();
-        println!("{:?}", encoded_data);
-        println!("{:?}", encode(&encoded_data));
+            let mut stream = RlpStream::new();
+            stream.begin_unbounded_list();
+            tx.rlp_append(&mut stream);
+            stream.finalize_unbounded_list();
+            let rlp_encoded = stream.out().freeze();
 
-        println!("{:?}", tx_sign_input.struct_hash().unwrap());
+            let rlp = Rlp::new(&rlp_encoded);
 
-        println!("{:?}", tx_sign_input.encode_eip712().unwrap());
+            println!("RLP ITEM COUNT: {:?}", rlp.item_count());
+            // Print 16 rlp.val_at
+            println!("nonce: {:?}", rlp.val_at::<U256>(0).unwrap());
+            println!(
+                "max_priority_fee_per_gas: {:?}",
+                rlp.val_at::<U256>(1).unwrap()
+            );
+            println!("gas_price: {:?}", rlp.val_at::<U256>(2).unwrap());
+            println!("gas_limit: {:?}", rlp.val_at::<U256>(3).unwrap());
+            println!("to: {:?}", rlp.val_at::<Address>(4).unwrap());
+            println!("value: {:?}", rlp.val_at::<U256>(5).unwrap());
+            println!("data: {:?}", rlp.val_at::<U256>(6).unwrap()); // Should be bytes
+            println!("v: {:?}", rlp.val_at::<U256>(7).unwrap());
+            println!("r: {:?}", rlp.val_at::<U256>(8).unwrap());
+            println!("s: {:?}", rlp.val_at::<U256>(9).unwrap());
+            println!("chain_id: {:?}", rlp.val_at::<U256>(10).unwrap());
+            println!("from: {:?}", rlp.val_at::<Address>(11).unwrap());
+            println!("gas_per_pub_data: {:?}", rlp.val_at::<U256>(12).unwrap());
+            // println!("factory_deps: {:?}", rlp.list_at(13).unwrap());
+            // println!("custom_signature: {:?}", rlp.val_at(14).unwrap());
+            // println!("paymaster_params: {:?}", rlp.val_at(15).unwrap());
+
+            println!(
+                "{:?}",
+                provider
+                    .send_raw_transaction(Bytes::from(
+                        [&[EIP712_TX_TYPE], &rlp_encoded[..]].concat()
+                    ))
+                    .await
+                    .unwrap()
+            );
+        }
     }
 }
