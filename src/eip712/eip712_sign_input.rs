36a7807bd3e347a09a19ea1b84a2c454e530127d
IAvecilla <iavecilla@fi.uba.ar>
2023-05-30 15:12:08 -0300

    Fix new line and test import

--- src/eip712/eip712_sign_input.rs
+++ src/eip712/eip712_sign_input.rs
@@ -141,7 +141,7 @@ mod tests {
     use super::*;
     use crate::{
         eip712::{
-            eip712_transaction_request::Eip712Meta,
+            eip712_transaction_request::{Eip712Meta, PaymasterParams},
             utils::{DEFAULT_GAS_PER_PUBDATA_LIMIT, EIP712_TX_TYPE},
             Eip712TransactionRequest,
         },
