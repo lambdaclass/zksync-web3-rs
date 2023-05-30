bf021890b195edb0c3e6dc65b9347354b4aed02e
Iván Litteri <ilitteri@fi.uba.ar>
2023-05-30 18:31:59 -0300

    Add `const`s to `zks_utils` module

--- src/zks_utils.rs
+++ src/zks_utils.rs
@@ -1,6 +1,12 @@
 /* Misc */
 
 pub const ERA_CHAIN_ID: u16 = 0x10E;
+pub const EIP712_TX_TYPE: u8 = 0x71;
+// The large L2 gas per pubdata to sign. This gas is enough to ensure that
+// any reasonable limit will be accepted. Note, that the operator is NOT required to
+// use the honest value of gas per pubdata and it can use any value up to the one signed by the user.
+// In the future releases, we will provide a way to estimate the current gasPerPubdata.
+pub const DEFAULT_GAS_PER_PUBDATA_LIMIT: u64 = 50000;
 
 /* Contracts */
 
