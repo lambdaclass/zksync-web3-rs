7bb3d25f136a5e24af72a893540c5e1878ff9d4b
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2023-05-31 14:26:55 -0300

    EIP712 RLP Encoding (#22)
    * Implement `rlp::Encoding` for `Eip712TransactionRequest`
    
    Co-authored-by: Nacho Avecilla <nachoavecilla@gmail.com>
    
    * Update `test_eip712`
    
    * Update `rlp` implementation
    
    * Update constant type
    
    * Fix structures
    
    * Fixes
    
    * EIP712 bug fixes (#27)
    
    * testito
    
    * Add stuff
    
    * `value` & `gas_price` are not optional
    
    * `verifying_contract` & `salt` should be `None` in the domain
    
    * Fix `struct_hash`
    
    * Implement `Default` for `PaymasterParams`
    
    * Chain id as constant
    
    * Update tests
    
    * `cargo fmt`
    
    * Add new line
    
    * Remove test file
    
    * Remove test file
    
    * Fix new line and test import
    
    ---------
    
    Co-authored-by: IAvecilla <iavecilla@fi.uba.ar>
    
    ---------
    
    Co-authored-by: Nacho Avecilla <nachoavecilla@gmail.com>
    Co-authored-by: IAvecilla <iavecilla@fi.uba.ar>

--- src/eip712/eip712_sign_input.rs
+++ src/eip712/eip712_sign_input.rs
@@ -33,14 +33,13 @@ pub struct Eip712SignInput {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub paymaster: Option<Address>,
     pub nonce: U256,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub value: Option<U256>,
+    pub value: U256,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<Bytes>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub factory_deps: Option<Vec<u8>>,
+    pub factory_deps: Option<Vec<Bytes>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub paymaster_input: Option<Vec<u8>>,
+    pub paymaster_input: Option<Bytes>,
 }
 
 // FIXME: Cleanup this.
@@ -48,7 +47,7 @@ pub fn eip712_sign_input_types() -> Types {
     let mut types = Types::new();
 
     types.insert(
-        "zkSync".to_string(),
+        "Transaction".to_string(),
         vec![
             Eip712DomainType {
                 name: "txType".to_string(),
@@ -56,11 +55,11 @@ pub fn eip712_sign_input_types() -> Types {
             },
             Eip712DomainType {
                 name: "from".to_string(),
-                r#type: "address".to_string(),
+                r#type: "uint256".to_string(),
             },
             Eip712DomainType {
                 name: "to".to_string(),
-                r#type: "address".to_string(),
+                r#type: "uint256".to_string(),
             },
             Eip712DomainType {
                 name: "gasLimit".to_string(),
@@ -80,7 +79,7 @@ pub fn eip712_sign_input_types() -> Types {
             },
             Eip712DomainType {
                 name: "paymaster".to_string(),
-                r#type: "address".to_string(),
+                r#type: "uint256".to_string(),
             },
             Eip712DomainType {
                 name: "nonce".to_string(),
@@ -96,7 +95,7 @@ pub fn eip712_sign_input_types() -> Types {
             },
             Eip712DomainType {
                 name: "factoryDeps".to_string(),
-                r#type: "bytes".to_string(),
+                r#type: "bytes32[]".to_string(),
             },
             Eip712DomainType {
                 name: "paymasterInput".to_string(),
@@ -104,9 +103,6 @@ pub fn eip712_sign_input_types() -> Types {
             },
         ],
     );
-    types.insert("uint256".to_string(), Vec::new());
-    types.insert("bytes".to_string(), Vec::new());
-
     types
 }
 
@@ -118,25 +114,25 @@ impl Eip712 for Eip712SignInput {
             name: Some(String::from("zkSync")),
             version: Some(String::from("2")),
             chain_id: Some(U256::from(270_i32)),
-            verifying_contract: "0xDAbb67b676F5b01FcC8997Cc8439846D0d8078ca".parse().ok(),
-            salt: Some([0_u8; 32]),
+            verifying_contract: None,
+            salt: None,
         })
     }
 
     fn type_hash() -> Result<[u8; 32], Self::Error> {
-        todo!()
+        Ok(keccak256(encode_type(
+            "Transaction",
+            &eip712_sign_input_types(),
+        )?))
     }
 
     fn struct_hash(&self) -> Result<[u8; 32], Self::Error> {
-        let types = eip712_sign_input_types();
-        let type_hash = keccak256(encode_type("zkSync", &types)?);
-        Ok(keccak256(
-            [
-                &type_hash,
-                &encode(&encode_data("zkSync", &json!(self), &types)?)[..],
-            ]
-            .concat(),
-        ))
+        let hash = keccak256(encode(&encode_data(
+            "Transaction",
+            &json!(self),
+            &eip712_sign_input_types(),
+        )?));
+        Ok(hash)
     }
 }
 
@@ -145,68 +141,187 @@ mod tests {
     use super::*;
     use crate::{
         eip712::{
-            eip712_transaction_request::Eip712Meta,
+            eip712_transaction_request::{Eip712Meta, PaymasterParams},
             utils::{DEFAULT_GAS_PER_PUBDATA_LIMIT, EIP712_TX_TYPE},
             Eip712TransactionRequest,
         },
         zks_provider::ZKSProvider,
-        zks_utils::CONTRACT_DEPLOYER_ADDR,
+        zks_utils::{CONTRACT_DEPLOYER_ADDR, ERA_CHAIN_ID},
     };
     use ethers::{
-        abi::encode,
+        abi::AbiEncode,
+        prelude::{k256::ecdsa::SigningKey, MiddlewareBuilder},
         providers::{Middleware, Provider},
+        signers::Signer,
+        signers::Wallet,
+        solc::{Artifact, Project, ProjectPathsConfig},
+        types::Signature,
         utils::keccak256,
     };
 
     #[tokio::test]
+    async fn testito2() {
+        let wallet = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110"
+            .parse::<Wallet<SigningKey>>()
+            .unwrap();
+
+        let provider = Provider::try_from(format!(
+            "http://{host}:{port}",
+            // host = "65.108.204.116",
+            host = "localhost",
+            port = 3050
+        ))
+        .unwrap()
+        .with_signer(wallet.clone());
+
+        let nonce = provider
+            .get_transaction_count(
+                "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049"
+                    .parse::<Address>()
+                    .unwrap(),
+                None,
+            )
+            .await
+            .unwrap()
+            .encode_hex();
+        let mut tx: Eip712TransactionRequest = serde_json::from_str(&format!(r#"{{
+            "chainId": "0x10E",
+            "nonce": "{nonce}",
+            "from": "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049",
+            "to": "0x0000000000000000000000000000000000008006",
+            "gas": "0x0",
+            "gasPrice": "0xED4A5100",
+            "maxPriorityFeePerGas": "0x5F5E100",
+            "value": "0x0",
+            "data": "0x9c4d535b00000000000000000000000000000000000000000000000000000000000000000100001bcf3424d9bc67cdb6eca8cfb731cec86df28064283f3c82fb1bf5c8be00000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000",
+            "type": "0x71",
+            "customData": {{
+                "gasPerPubdata": "0xC350",
+                "customSignature": null,
+                "factoryDeps": [
+                    "0x000200000000000200010000000103550000006001100270000000130010019d0000008001000039000000400010043f0000000101200190000000290000c13d0000000001000031000000040110008c000000420000413d0000000101000367000000000101043b000000e001100270000000150210009c000000310000613d000000160110009c000000420000c13d0000000001000416000000000110004c000000420000c13d000000040100008a00000000011000310000001702000041000000200310008c000000000300001900000000030240190000001701100197000000000410004c000000000200a019000000170110009c00000000010300190000000001026019000000000110004c000000420000c13d00000004010000390000000101100367000000000101043b000000000010041b0000000001000019000000490001042e0000000001000416000000000110004c000000420000c13d0000002001000039000001000010044300000120000004430000001401000041000000490001042e0000000001000416000000000110004c000000420000c13d000000040100008a00000000011000310000001702000041000000000310004c000000000300001900000000030240190000001701100197000000000410004c000000000200a019000000170110009c00000000010300190000000001026019000000000110004c000000440000613d00000000010000190000004a00010430000000000100041a000000800010043f0000001801000041000000490001042e0000004800000432000000490001042e0000004a00010430000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0000000200000000000000000000000000000040000001000000000000000000000000000000000000000000000000000000000000000000000000006d4ce63c0000000000000000000000000000000000000000000000000000000060fe47b1800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000008000000000000000000000000000000000000000000000000000000000000000000000000000000000d5c7d2782d356f4a1a2e458d242d21e07a04810c9f771eed6501083e07288c87"
+                ],
+                "paymasterParams": {{
+                    "paymaster": "0x0000000000000000000000000000000000000000",
+                    "paymasterInput": "0x"
+                }}
+            }}
+        }}"#)).unwrap();
+
+        let fee = provider.estimate_fee(tx.clone()).await.unwrap();
+
+        tx.max_fee_per_gas = Some(fee.max_fee_per_gas);
+        tx.gas_limit = Some(U256::from("0x2d611"));
+
+        let eip712: Eip712SignInput = tx.clone().into();
+
+        if let Some(custom_data) = &mut tx.custom_data {
+            let signature: Signature = Wallet::sign_typed_data(&wallet, &eip712).await.unwrap();
+            let signature_bytes = Bytes::from(signature.to_vec());
+            custom_data.custom_signature = Some(signature_bytes);
+        }
+
+        let eip712: Eip712SignInput = tx.clone().into();
+        let signed_msg = wallet.sign_typed_data(&eip712).await.unwrap();
+        let unsigned_rlp_encoded = tx.rlp_signed(signed_msg);
+        let deployment_transaction_receipt = provider
+            .send_raw_transaction(
+                [&[EIP712_TX_TYPE], &unsigned_rlp_encoded[..]]
+                    .concat()
+                    .into(),
+            )
+            .await
+            .unwrap()
+            .await
+            .unwrap()
+            .unwrap();
+        println!("TRANSACTION RECEIPT {:?}", deployment_transaction_receipt);
+        println!(
+            "TRANSACTION HASH {:?}",
+            deployment_transaction_receipt.transaction_hash
+        );
+        println!(
+            "CONTRACT ADDRESS {:?}",
+            deployment_transaction_receipt.contract_address
+        );
+    }
+
+    #[tokio::test]
     async fn test_eip712() {
+        /* Create Wallet */
+
+        let mut wallet = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110"
+            .parse::<Wallet<SigningKey>>()
+            .unwrap();
+
         /* Connect to node */
 
         let provider = Provider::try_from(format!(
             "http://{host}:{port}",
-            host = "65.108.204.116",
+            // host = "65.108.204.116",
+            host = "localhost",
             port = 3050
         ))
-        .unwrap();
+        .unwrap()
+        .with_signer(wallet.clone());
 
         /* Create Transaction */
 
         let mut tx = Eip712TransactionRequest::default();
 
         tx.r#type = EIP712_TX_TYPE.into();
-        tx.from = "0xbd29A1B981925B94eEc5c4F1125AF02a2Ec4d1cA".parse().ok();
+        tx.from = "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049".parse().ok();
         tx.to = CONTRACT_DEPLOYER_ADDR.parse().ok();
-        tx.chain_id = 270.into();
+        tx.chain_id = ERA_CHAIN_ID.into();
+        tx.nonce = provider
+            .get_transaction_count(tx.from.unwrap(), None)
+            .await
+            .unwrap();
+        tx.value = U256::zero();
+        tx.gas_price = provider.get_gas_price().await.unwrap();
 
-        // let fee = provider.estimate_fee(tx.clone()).await.unwrap();
-
-        // tx.max_priority_fee_per_gas = Some(fee.max_priority_fee_per_gas);
-        // tx.max_fee_per_gas = Some(fee.max_fee_per_gas);
-        // tx.gas_limit = Some(fee.gas_limit);
-
-        // Build data
-        let build_data = |function_signature: &str| -> eyre::Result<Vec<u8>> {
+        let build_data = |function_signature: &str| -> Bytes {
             // See https://docs.soliditylang.org/en/latest/abi-spec.html#examples
             // TODO: Support all kind of function calls and return cast
             // (nowadays we only support empty function calls).
-            Ok(keccak256(function_signature.as_bytes())
-                .get(0..4)
-                .unwrap()
-                .to_vec())
+            Bytes::from(
+                keccak256(function_signature.as_bytes())
+                    .get(0..4)
+                    .unwrap()
+                    .to_vec(),
+            )
         };
-
-        tx.data = Some(build_data("create()").unwrap().into());
+        tx.data = Some(build_data("create"));
+        tx.data = Some(Bytes::from(hex::decode("9c4d535b00000000000000000000000000000000000000000000000000000000000000000100001bcf3424d9bc67cdb6eca8cfb731cec86df28064283f3c82fb1bf5c8be00000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000").unwrap()));
 
         // Build custom data
-        let paymaster_contract = provider.get_testnet_paymaster().await.unwrap();
-        let paymaster_contract_bytecode =
-            provider.get_code(paymaster_contract, None).await.unwrap();
+        let paths =
+            ProjectPathsConfig::builder().build_with_root("./src/compile/test_contracts/test");
+        let project = Project::builder()
+            .paths(paths)
+            .set_auto_detect(true)
+            .no_artifacts()
+            .build()
+            .unwrap();
+        let compilation_output = project.compile().unwrap();
+        let contract = compilation_output.find_first("Test").unwrap().clone();
+        let (_, bytecode, _) = contract.into_parts();
 
         let mut custom_data = Eip712Meta::default();
-        custom_data.factory_deps = Some(vec![paymaster_contract_bytecode]);
+        custom_data.factory_deps = Some(vec![[
+            bytecode.unwrap().to_vec(),
+            hex::decode("000000000000000000000000000000").unwrap(),
+        ]
+        .concat()
+        .into()]);
         custom_data.gas_per_pubdata = DEFAULT_GAS_PER_PUBDATA_LIMIT.into();
-
+        custom_data.paymaster_params = Some(PaymasterParams::default());
         tx.custom_data = Some(custom_data);
+
+        let fee = provider.estimate_fee(tx.clone()).await.unwrap();
+        tx.max_priority_fee_per_gas = Some(fee.max_priority_fee_per_gas);
+        tx.max_fee_per_gas = Some(fee.max_fee_per_gas);
+        tx.gas_limit = Some(fee.gas_limit);
 
         /* Create Sign Input */
 
@@ -214,20 +329,30 @@ mod tests {
 
         tx_sign_input.gas_per_pubdata_byte_limit = Some(DEFAULT_GAS_PER_PUBDATA_LIMIT.into());
 
-        /* Testing */
+        /* Update Wallet */
 
-        let encoded_type = encode_type("zkSync", &eip712_sign_input_types()).unwrap();
-        let type_hash = keccak256(&encoded_type);
-        println!("{:?}", encoded_type);
-        println!("{:?}", type_hash);
+        wallet = Wallet::with_chain_id(
+            wallet,
+            tx_sign_input.domain().unwrap().chain_id.unwrap().as_u64(),
+        );
 
-        let encoded_data =
-            encode_data("zkSync", &json!(tx_sign_input), &eip712_sign_input_types()).unwrap();
-        println!("{:?}", encoded_data);
-        println!("{:?}", encode(&encoded_data));
+        if let Some(custom_data) = &mut tx.custom_data {
+            let signature: Signature = Wallet::sign_typed_data(&wallet, &tx_sign_input)
+                .await
+                .unwrap();
+            let signature_bytes = Bytes::from(signature.to_vec());
+            custom_data.custom_signature = Some(signature_bytes);
+        }
 
-        println!("{:?}", tx_sign_input.struct_hash().unwrap());
+        println!("{tx:#?}");
 
-        println!("{:?}", tx_sign_input.encode_eip712().unwrap());
+        /* Transaction Signing */
+        println!(
+            "{:?}",
+            provider
+                .send_raw_transaction([&[EIP712_TX_TYPE], &tx.rlp_unsigned()[..]].concat().into())
+                .await
+                .unwrap()
+        );
     }
 }
--- src/eip712/eip712_transaction_request.rs
+++ src/eip712/eip712_transaction_request.rs
@@ -1,45 +1,97 @@
-use super::{hash_bytecode, utils, Eip712SignInput};
-use ethers::types::{transaction::eip2930::AccessList, Address, Bytes, U256};
+use super::{
+    hash_bytecode,
+    utils::{self, rlp_opt},
+    Eip712SignInput,
+};
+use ethers::{
+    types::{transaction::eip2930::AccessList, Address, Bytes, Signature, U256, U64},
+    utils::rlp::{Encodable, RlpStream},
+};
 use serde::{Deserialize, Serialize};
 
 // TODO: Not all the fields are optional. This was copied from the JS implementation.
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
 pub struct Eip712TransactionRequest {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub to: Option<Address>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub from: Option<Address>,
     pub nonce: U256,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub gas_limit: Option<U256>,
-    pub gas_price: Option<U256>,
+    pub gas_price: U256,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<Bytes>,
-    pub value: Option<U256>,
+    pub value: U256,
     pub chain_id: U256,
     pub r#type: U256,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub access_list: Option<AccessList>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_priority_fee_per_gas: Option<U256>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_fee_per_gas: Option<U256>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub custom_data: Option<Eip712Meta>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub ccip_read_enabled: Option<bool>,
 }
 
-// TODO: Implement Default for Eip712TransactionRequest.
-// impl Default for Eip712TransactionRequest {}
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
-#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
-pub struct Eip712Meta {
-    pub gas_per_pubdata: U256,
-    pub factory_deps: Option<Vec<Bytes>>,
-    // TODO: Is this field optional?
-    pub custom_signature: Option<Bytes>,
-    // TODO: Is this field optional?
-    pub paymaster_params: Option<PaymasterParams>,
-}
+impl Eip712TransactionRequest {
+    pub fn rlp_unsigned(&self) -> Bytes {
+        self.rlp(None)
+    }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
-pub struct PaymasterParams {
-    pub paymaster: Address,
-    pub paymaster_input: Vec<u8>,
+    pub fn rlp_signed(&self, signature: Signature) -> Bytes {
+        self.rlp(Some(signature))
+    }
+
+    pub fn rlp(&self, signature: Option<Signature>) -> Bytes {
+        let mut stream = RlpStream::new();
+        stream.begin_unbounded_list();
+
+        // 0
+        stream.append(&self.nonce);
+        // 1
+        rlp_opt(&mut stream, &self.max_priority_fee_per_gas);
+        // 2
+        rlp_opt(&mut stream, &self.max_fee_per_gas);
+        // 3 (supped to be gas)
+        rlp_opt(&mut stream, &self.gas_limit);
+        // 4
+        rlp_opt(&mut stream, &self.to);
+        // 5
+        stream.append(&self.value);
+        // 6
+        rlp_opt(&mut stream, &self.data.clone().map(|d| d.0));
+        if let Some(signature) = signature {
+            // 7
+            stream.append(&U64::from(signature.v));
+            // 8
+            stream.append(&signature.r);
+            // 9
+            stream.append(&signature.s);
+        } else {
+            // 7, 8, 9 must be set even if no signature is provided.
+            // This should be the case of transaction that have a
+            // custom signature set.
+            stream.append(&"");
+            stream.append(&"");
+            stream.append(&"");
+        }
+        // 10
+        stream.append(&self.chain_id);
+        // 11
+        rlp_opt(&mut stream, &self.from);
+        if let Some(meta) = &self.custom_data {
+            // 12, 13, 14, 15
+            meta.rlp_append(&mut stream);
+        }
+
+        stream.finalize_unbounded_list();
+        stream.out().freeze().into()
+    }
 }
 
 impl Into<Eip712SignInput> for Eip712TransactionRequest {
@@ -61,8 +113,9 @@ impl Into<Eip712SignInput> for Eip712TransactionRequest {
         eip712_sign_input.data = self.data;
 
         if let Some(custom_data) = self.custom_data {
-            eip712_sign_input.factory_deps =
-                Some(hash_bytecode(custom_data.factory_deps).unwrap().to_vec());
+            eip712_sign_input.factory_deps = Some(vec![hash_bytecode(custom_data.factory_deps)
+                .unwrap()
+                .into()]);
             eip712_sign_input.gas_per_pubdata_byte_limit =
                 Some(U256::from(utils::DEFAULT_GAS_PER_PUBDATA_LIMIT));
             if let Some(paymaster_params) = custom_data.paymaster_params {
@@ -74,10 +127,59 @@ impl Into<Eip712SignInput> for Eip712TransactionRequest {
                         .parse()
                         .unwrap(),
                 );
-                eip712_sign_input.paymaster_input = Some(vec![0]);
+                // TODO: This default seems to be wrong.
+                eip712_sign_input.paymaster_input = Some(Bytes::default());
             }
         }
 
         eip712_sign_input
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
+pub struct Eip712Meta {
+    pub gas_per_pubdata: U256,
+    pub factory_deps: Option<Vec<Bytes>>,
+    pub custom_signature: Option<Bytes>,
+    pub paymaster_params: Option<PaymasterParams>,
+}
+
+impl Encodable for Eip712Meta {
+    fn rlp_append(&self, stream: &mut ethers::utils::rlp::RlpStream) {
+        // 12
+        stream.append(&self.gas_per_pubdata);
+        // 13
+        if let Some(factory_deps) = &self.factory_deps {
+            stream.begin_list(factory_deps.len());
+            for dep in factory_deps.iter() {
+                stream.append(&dep.to_vec());
+            }
+        } else {
+            stream.begin_list(0);
+        }
+        // 14
+        rlp_opt(stream, &self.custom_signature.clone().map(|s| s.to_vec()));
+        // 15
+        if let Some(paymaster_params) = &self.paymaster_params {
+            paymaster_params.rlp_append(stream);
+        } else {
+            stream.begin_list(0);
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
+pub struct PaymasterParams {
+    pub paymaster: Address,
+    pub paymaster_input: Bytes,
+}
+
+impl Encodable for PaymasterParams {
+    fn rlp_append(&self, stream: &mut ethers::utils::rlp::RlpStream) {
+        stream.begin_list(2);
+        stream.append(&self.paymaster.as_bytes());
+        stream.append(&self.paymaster_input.to_vec());
     }
 }
