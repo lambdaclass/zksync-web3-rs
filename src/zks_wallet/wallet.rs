ba4d35e2d7ef1068f1ab1dcb4e5737949b1debc1
Iván Litteri <ilitteri@fi.uba.ar>
2023-06-01 12:47:56 -0300

    Refactor deployment-related methods, improve tests

--- src/zks_wallet/wallet.rs
+++ src/zks_wallet/wallet.rs
@@ -200,8 +200,6 @@ where
         Ok(transaction_receipt)
     }
 
-    // The field `constant` of ethers::abi::Function is deprecated.
-    #[allow(deprecated)]
     pub async fn deploy(
         &self,
         contract_bytecode: Bytes,
@@ -210,89 +208,9 @@ where
     where
         M: ZKSProvider,
     {
-        let era_provider = match &self.era_provider {
-            Some(era_provider) => era_provider,
-            None => return Err(ZKSWalletError::CustomError("no era provider".to_string())),
-        };
-
-        let custom_data = Eip712Meta::new().factory_deps({
-            let mut factory_deps = vec![contract_bytecode.clone()];
-            if let Some(contract_dependencies) = contract_dependencies {
-                factory_deps.extend(contract_dependencies);
-            }
-            factory_deps
-        });
-
-        let mut deploy_request = Eip712TransactionRequest::new()
-            .from(self.address())
-            .to(Address::from_str(CONTRACT_DEPLOYER_ADDR).unwrap())
-            .nonce(
-                era_provider
-                    .get_transaction_count(self.address(), None)
-                    .await?,
-            )
-            .gas_price(era_provider.get_gas_price().await?)
-            .data({
-                let create = ethers::abi::Function {
-                    name: "create".to_owned(),
-                    inputs: vec![
-                        Param {
-                            name: "_salt".to_owned(),
-                            kind: ParamType::FixedBytes(32),
-                            internal_type: None,
-                        },
-                        Param {
-                            name: "_bytecodeHash".to_owned(),
-                            kind: ParamType::FixedBytes(32),
-                            internal_type: None,
-                        },
-                        Param {
-                            name: "_input".to_owned(),
-                            kind: ParamType::Bytes,
-                            internal_type: None,
-                        },
-                    ],
-                    outputs: vec![],
-                    state_mutability: ethers::abi::StateMutability::Payable,
-                    constant: None,
-                };
-
-                // TODO: User could provide this instead of defaulting.
-                let salt = [0_u8; 32];
-                let bytecode_hash = hash_bytecode(&contract_bytecode)?;
-                // TODO: User could provide this instead of defaulting.
-                let call_data = Bytes::default();
-
-                encode_function_data(&create, (salt, bytecode_hash, call_data))?
-            })
-            .custom_data(custom_data.clone());
-
-        let fee = era_provider.estimate_fee(deploy_request.clone()).await?;
-        deploy_request = deploy_request
-            .max_priority_fee_per_gas(fee.max_priority_fee_per_gas)
-            .max_fee_per_gas(fee.max_fee_per_gas)
-            .gas_limit(fee.gas_limit);
-
-        let signable_data: Eip712SignInput = deploy_request.clone().try_into()?;
-        let signature: Signature = self.wallet.sign_typed_data(&signable_data).await?;
-        deploy_request =
-            deploy_request.custom_data(custom_data.custom_signature(signature.to_vec()));
-
-        let pending_transaction = era_provider
-            .send_raw_transaction(
-                [&[EIP712_TX_TYPE], &deploy_request.rlp_unsigned()[..]]
-                    .concat()
-                    .into(),
-            )
+        let transaction_receipt = self
+            ._deploy(contract_bytecode, contract_dependencies)
             .await?;
-
-        // TODO: Should we wait here for the transaction to be confirmed on-chain?
-
-        let transaction_receipt = pending_transaction
-            .await?
-            .ok_or(ZKSWalletError::CustomError(
-                "no transaction receipt".to_string(),
-            ))?;
 
         let contract_address =
             transaction_receipt
@@ -304,13 +222,35 @@ where
         Ok(contract_address)
     }
 
-    // The field `constant` of ethers::abi::Function is deprecated.
-    #[allow(deprecated)]
     pub async fn deploy_with_receipt(
         &self,
         contract_bytecode: Bytes,
         contract_dependencies: Option<Vec<Bytes>>,
     ) -> Result<(Address, TransactionReceipt), ZKSWalletError<M, D>>
+    where
+        M: ZKSProvider,
+    {
+        let transaction_receipt = self
+            ._deploy(contract_bytecode, contract_dependencies)
+            .await?;
+
+        let contract_address =
+            transaction_receipt
+                .contract_address
+                .ok_or(ZKSWalletError::CustomError(
+                    "no contract address".to_string(),
+                ))?;
+
+        Ok((contract_address, transaction_receipt))
+    }
+
+    // The field `constant` of ethers::abi::Function is deprecated.
+    #[allow(deprecated)]
+    async fn _deploy(
+        &self,
+        contract_bytecode: Bytes,
+        contract_dependencies: Option<Vec<Bytes>>,
+    ) -> Result<TransactionReceipt, ZKSWalletError<M, D>>
     where
         M: ZKSProvider,
     {
@@ -394,20 +334,11 @@ where
 
         // TODO: Should we wait here for the transaction to be confirmed on-chain?
 
-        let transaction_receipt = pending_transaction
+        pending_transaction
             .await?
             .ok_or(ZKSWalletError::CustomError(
                 "no transaction receipt".to_string(),
-            ))?;
-
-        let contract_address =
-            transaction_receipt
-                .contract_address
-                .ok_or(ZKSWalletError::CustomError(
-                    "no contract address".to_string(),
-                ))?;
-
-        Ok((contract_address, transaction_receipt))
+            ))
     }
 }
 
@@ -560,8 +491,29 @@ mod zks_signer_tests {
         let zk_wallet = ZKSWallet::new(wallet, Some(era_provider.clone()), None).unwrap();
         let contract_bytecode = Bytes::from(hex::decode("000200000000000200010000000103550000006001100270000000130010019d0000008001000039000000400010043f0000000101200190000000290000c13d0000000001000031000000040110008c000000420000413d0000000101000367000000000101043b000000e001100270000000150210009c000000310000613d000000160110009c000000420000c13d0000000001000416000000000110004c000000420000c13d000000040100008a00000000011000310000001702000041000000200310008c000000000300001900000000030240190000001701100197000000000410004c000000000200a019000000170110009c00000000010300190000000001026019000000000110004c000000420000c13d00000004010000390000000101100367000000000101043b000000000010041b0000000001000019000000490001042e0000000001000416000000000110004c000000420000c13d0000002001000039000001000010044300000120000004430000001401000041000000490001042e0000000001000416000000000110004c000000420000c13d000000040100008a00000000011000310000001702000041000000000310004c000000000300001900000000030240190000001701100197000000000410004c000000000200a019000000170110009c00000000010300190000000001026019000000000110004c000000440000613d00000000010000190000004a00010430000000000100041a000000800010043f0000001801000041000000490001042e0000004800000432000000490001042e0000004a00010430000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0000000200000000000000000000000000000040000001000000000000000000000000000000000000000000000000000000000000000000000000006d4ce63c0000000000000000000000000000000000000000000000000000000060fe47b1800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000008000000000000000000000000000000000000000000000000000000000000000000000000000000000d5c7d2782d356f4a1a2e458d242d21e07a04810c9f771eed6501083e07288c87").unwrap());
 
-        let (_contract_address, deploy_receipt) = zk_wallet
-            .deploy_with_receipt(contract_bytecode, None)
+        let contract_address = zk_wallet
+            .deploy(contract_bytecode.clone(), None)
+            .await
+            .unwrap();
+
+        let recovered_bytecode = era_provider.get_code(contract_address, None).await.unwrap();
+
+        assert_eq!(contract_bytecode, recovered_bytecode);
+    }
+
+    #[tokio::test]
+    async fn test_deploy_with_receipt() {
+        let deployer_private_key =
+            "0x28a574ab2de8a00364d5dd4b07c4f2f574ef7fcc2a86a197f65abaec836d1959";
+        let era_provider = era_provider();
+        let wallet = LocalWallet::from_str(deployer_private_key)
+            .unwrap()
+            .with_chain_id(ERA_CHAIN_ID);
+        let zk_wallet = ZKSWallet::new(wallet, Some(era_provider.clone()), None).unwrap();
+        let contract_bytecode = Bytes::from(hex::decode("000200000000000200010000000103550000006001100270000000130010019d0000008001000039000000400010043f0000000101200190000000290000c13d0000000001000031000000040110008c000000420000413d0000000101000367000000000101043b000000e001100270000000150210009c000000310000613d000000160110009c000000420000c13d0000000001000416000000000110004c000000420000c13d000000040100008a00000000011000310000001702000041000000200310008c000000000300001900000000030240190000001701100197000000000410004c000000000200a019000000170110009c00000000010300190000000001026019000000000110004c000000420000c13d00000004010000390000000101100367000000000101043b000000000010041b0000000001000019000000490001042e0000000001000416000000000110004c000000420000c13d0000002001000039000001000010044300000120000004430000001401000041000000490001042e0000000001000416000000000110004c000000420000c13d000000040100008a00000000011000310000001702000041000000000310004c000000000300001900000000030240190000001701100197000000000410004c000000000200a019000000170110009c00000000010300190000000001026019000000000110004c000000440000613d00000000010000190000004a00010430000000000100041a000000800010043f0000001801000041000000490001042e0000004800000432000000490001042e0000004a00010430000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0000000200000000000000000000000000000040000001000000000000000000000000000000000000000000000000000000000000000000000000006d4ce63c0000000000000000000000000000000000000000000000000000000060fe47b1800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000008000000000000000000000000000000000000000000000000000000000000000000000000000000000d5c7d2782d356f4a1a2e458d242d21e07a04810c9f771eed6501083e07288c87").unwrap());
+
+        let (contract_address, deploy_receipt) = zk_wallet
+            .deploy_with_receipt(contract_bytecode.clone(), None)
             .await
             .unwrap();
 
@@ -570,5 +522,9 @@ mod zks_signer_tests {
             .get_transaction(deploy_receipt.transaction_hash)
             .await
             .is_ok());
+
+        let recovered_bytecode = era_provider.get_code(contract_address, None).await.unwrap();
+
+        assert_eq!(contract_bytecode, recovered_bytecode);
     }
 }
