121e0167ebcef8afe00491fe810495da41d4288f
Iván Litteri <ilitteri@fi.uba.ar>
2023-06-06 18:54:18 -0300

    Fix helper functions

--- src/eip712/mod.rs
+++ src/eip712/mod.rs
@@ -1,4 +1,7 @@
-use ethers::types::{transaction::eip712::Eip712Error, Bytes};
+use ethers::{
+    types::{transaction::eip712::Eip712Error, Bytes},
+    utils::rlp::{Encodable, RlpStream},
+};
 use sha2::Digest;
 use std::num::TryFromIntError;
 
@@ -19,7 +22,7 @@ pub use paymaster_params::PaymasterParams;
 /// * The first 2 bytes denote the version of bytecode hash format and are currently equal to [1,0].
 /// * The second 2 bytes denote the length of the bytecode in 32-byte words.
 /// * The rest of the 28-byte (i.e. 28 low big-endian bytes) are equal to the last 28 bytes of the sha256 hash of the contract's bytecode.
-pub fn hash_bytecode(bytecode: &Bytes) -> Result<[u8; 32], Eip712Error> {
+pub fn hash_bytecode(bytecode: &[u8]) -> Result<[u8; 32], Eip712Error> {
     let step_1: [u8; 2] = 0x0100_u16.to_be_bytes();
     let bytecode_length: u16 = (bytecode.len() / 32)
         .try_into()
@@ -44,4 +47,15 @@ pub fn hash_bytecode(bytecode: &Bytes) -> Result<[u8; 32], Eip712Error> {
         })?;
 
     Ok(contract_hash)
+}
+
+pub(crate) fn rlp_append_option<T>(stream: &mut RlpStream, value: Option<T>)
+where
+    T: Encodable,
+{
+    if let Some(v) = value {
+        stream.append(&v);
+    } else {
+        stream.append(&"");
+    }
 }
