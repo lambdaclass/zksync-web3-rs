38cd2d88c24c01cf91548bba91aa9c1d8c00772d
Iván Litteri <ilitteri@fi.uba.ar>
2023-06-16 11:34:24 -0300

    Update `call` cmd

--- src/cli/commands/call.rs
+++ src/cli/commands/call.rs
@@ -1,17 +1,13 @@
-use crate::cli::{commands::L1_CHAIN_ID, ZKSyncWeb3Config};
+use crate::ZKSWallet;
+use crate::cli::ZKSyncWeb3Config;
+use crate::zks_utils::ERA_CHAIN_ID;
 use crate::{
-    prelude::{k256::ecdsa::SigningKey, MiddlewareBuilder, SignerMiddleware},
-    providers::{Middleware, Provider},
+    providers::Provider,
     signers::Signer,
-    signers::Wallet,
-    types::{
-        transaction::eip2718::TypedTransaction, Address, Eip1559TransactionRequest,
-        TransactionReceipt,
-    },
-    utils::keccak256,
+    types::Address,
 };
 use clap::Args;
-use eyre::ContextCompat;
+use ethers::signers::LocalWallet;
 
 // TODO: Optional parameters were omitted, they should be added in the future.
 #[derive(Args)]
@@ -23,44 +19,23 @@ pub(crate) struct Call {
     #[clap(short, long, name = "FUNCTION_ARGS")]
     pub args: Option<Vec<String>>,
     #[clap(short, long, name = "PRIVATE_KEY")]
-    pub private_key: Option<String>,
+    pub private_key: LocalWallet,
 }
 
 pub(crate) async fn run(args: Call, config: ZKSyncWeb3Config) -> eyre::Result<()> {
-    let build_data = |function_signature: &str| -> eyre::Result<Vec<u8>> {
-        // See https://docs.soliditylang.org/en/latest/abi-spec.html#examples
-        // TODO: Support all kind of function calls and return cast
-        // (nowadays we only support empty function calls).
-        Ok(keccak256(function_signature.as_bytes())
-            .get(0..4)
-            .context("Couldn't get function signature's first four bytes")?
-            .to_vec())
-    };
-
-    let request = Eip1559TransactionRequest::new()
-        .to(args.contract)
-        .data(build_data(&args.function)?);
-    let mut transaction: TypedTransaction = request.into();
     let provider = Provider::try_from(format!(
         "http://{host}:{port}",
         host = config.host,
         port = config.port
-    ))?
-    .interval(std::time::Duration::from_millis(10));
-    if let Some(pk) = args.private_key {
-        let mut signer = pk.parse::<Wallet<SigningKey>>()?;
-        signer = Wallet::with_chain_id(signer, L1_CHAIN_ID);
-        let provider = provider.clone().with_signer(signer);
-        provider.fill_transaction(&mut transaction, None).await?;
-        let response: TransactionReceipt =
-            SignerMiddleware::send_transaction(&provider, transaction, None)
-                .await?
-                .await?
-                .context("No pending transaction")?;
-        log::info!("{:?}", response);
-    } else {
-        let response = provider.call(&transaction, None).await?;
-        log::info!("{:?}", response);
-    };
+    ))?;
+    let wallet = args.private_key.with_chain_id(ERA_CHAIN_ID);
+    let zk_wallet = ZKSWallet::new(wallet, Some(provider.clone()), None)?;
+
+    // TODO: Figure out how to parse the args correctly.
+    let output = zk_wallet
+        .call(args.contract, &args.function, args.args)
+        .await
+        .unwrap();
+    println!("{output:?}");
     Ok(())
 }
