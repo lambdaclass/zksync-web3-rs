1da3ba42dc6d9a10b4dad257e3f734cf7ff3e7c2
Martin Paulucci <martin.paulucci@lambdaclass.com>
2023-07-10 19:44:04 +0200

    Format code.

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -14,13 +14,11 @@ pub(crate) struct CompiledContract {
 }
 
 pub fn eth_provider() -> Provider<Http> {
-    let url: String =
-        env::var("ZKSYNC_WEB3_RS_L1_PROVIDER_URL").unwrap();
+    let url: String = env::var("ZKSYNC_WEB3_RS_L1_PROVIDER_URL").unwrap();
     Provider::try_from(url).unwrap()
 }
 
 pub fn era_provider() -> Provider<Http> {
-    let url: String =
-        env::var("ZKSYNC_WEB3_RS_L2_PROVIDER_URL").unwrap();
+    let url: String = env::var("ZKSYNC_WEB3_RS_L2_PROVIDER_URL").unwrap();
     Provider::try_from(url).unwrap()
 }
