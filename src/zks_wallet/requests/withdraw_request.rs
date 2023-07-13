01789f839e221d596451eba137c89c3a2b6b3fe5
IAvecilla <iavecilla@fi.uba.ar>
2023-07-13 19:11:36 -0300

    Add defaults for withdraw request

--- src/zks_wallet/requests/withdraw_request.rs
+++ src/zks_wallet/requests/withdraw_request.rs
@@ -1,41 +1,29 @@
-use ethers::types::{transaction::eip712::Eip712Error, Address, U256};
+use ethers::types::{Address, Bytes, U256};
 use std::fmt::Debug;
-
-use crate::eip712::Eip712TransactionRequest;
 
 #[derive(Clone, Debug)]
 pub struct WithdrawRequest {
     pub amount: U256,
-    pub to: Option<Address>,
-    pub from: Option<Address>,
+    pub to: Address,
+    pub from: Address,
 }
 
 impl WithdrawRequest {
     pub fn with(amount: U256) -> Self {
         Self {
             amount,
-            to: None,
-            from: None,
+            to: Default::default(),
+            from: Default::default(),
         }
     }
 
     pub fn to(mut self, to: Address) -> Self {
-        self.to = Some(to);
+        self.to = to;
         self
     }
 
     pub fn from(mut self, from: Address) -> Self {
-        self.from = Some(from);
+        self.from = from;
         self
     }
 }
-
-// impl TryFrom<Eip712TransactionRequest> for WithdrawRequest {
-//     type Error = Eip712Error;
-
-//     fn try_from(value: Eip712TransactionRequest) -> Result<Self, Self::Error> {
-//         Eip712TransactionRequest {
-
-//         }
-//     }
-// }
