c99f08265ff40ace26f4e075f4ce90aceec6cf8b
IAvecilla <iavecilla@fi.uba.ar>
2023-07-21 19:14:24 -0300

    Update payment example

--- examples/simple_payment/main.rs
+++ examples/simple_payment/main.rs
@@ -1,19 +1,16 @@
 use clap::Parser;
 use ethers::{
     abi::Address,
-    middleware::SignerMiddleware,
-    prelude::{k256::ecdsa::SigningKey, MiddlewareBuilder},
+    prelude::k256::ecdsa::SigningKey,
     providers::{Middleware, Provider},
     signers::{Signer, Wallet},
-    types::{
-        transaction::eip2718::TypedTransaction, Eip1559TransactionRequest, TransactionReceipt, U256,
-    },
+    types::{TransactionReceipt, U256},
 };
-use zksync_web3_rs::zks_provider::ZKSProvider;
+use zksync_web3_rs::ZKSWallet;
 
 // It is set so that the transaction is replay-protected (EIP-155)
 // https://era.zksync.io/docs/api/hardhat/testing.html#connect-wallet-to-local-nodes
-const L1_CHAIN_ID: u64 = 9;
+//const L1_CHAIN_ID: u64 = 9;
 const L2_CHAIN_ID: u64 = 270;
 
 #[derive(Parser)]
@@ -23,19 +20,13 @@ struct Args {
     #[clap(short, long)]
     pub port: u16,
     #[clap(long, name = "AMOUNT_TO_TRANSFER")]
-    pub amount: U256,
+    pub amount: String,
     #[clap(long, name = "SENDER_ADDRESS")]
     pub from: Address,
     #[clap(long, name = "RECEIVER_ADDRESS")]
     pub to: Address,
     #[clap(long, name = "SENDER_PRIVATE_KEY")]
     pub private_key: Wallet<SigningKey>,
-    #[clap(
-        long,
-        name = "NETWORK_NAME",
-        help = "Available networks: \"era\" or \"eth\""
-    )]
-    pub network: String,
 }
 
 #[tokio::main]
@@ -48,15 +39,10 @@ async fn main() {
     let args = Args::parse();
 
     /* Connecting to the node */
+    let amount = U256::from_dec_str(&args.amount).unwrap();
+    log::debug!("Amount to transfer: {:?}", amount);
+    let signer = Wallet::with_chain_id(args.private_key, L2_CHAIN_ID);
 
-    let signer = Wallet::with_chain_id(
-        args.private_key,
-        if args.network == "eth" {
-            L1_CHAIN_ID
-        } else {
-            L2_CHAIN_ID
-        },
-    );
     let provider = Provider::try_from(format!(
         "http://{host}:{port}",
         host = args.host,
@@ -64,46 +50,24 @@ async fn main() {
     ))
     .unwrap()
     .interval(std::time::Duration::from_millis(10));
-    let signer_middleware = provider.clone().with_signer(signer);
 
-    /* Payment transaction building */
-
-    let mut payment_request = Eip1559TransactionRequest::new()
-        .from(args.from)
-        .to(args.to)
-        .value(args.amount);
-
-    if args.network == "era" {
-        let fee = provider
-            .estimate_fee(payment_request.clone())
-            .await
-            .unwrap();
-        payment_request = payment_request.max_priority_fee_per_gas(fee.max_priority_fee_per_gas);
-        payment_request = payment_request.max_fee_per_gas(fee.max_fee_per_gas);
-    }
-
-    let transaction: TypedTransaction = payment_request.into();
-
-    log::debug!("{:?}", transaction);
+    let zk_wallet = ZKSWallet::new(signer, None, Some(provider.clone()), None).unwrap();
 
     /* Sending the payment transaction */
 
     log::debug!(
         "Sender's balance before paying: {:?}",
-        provider.get_balance(args.from, None).await.unwrap()
+        provider.clone().get_balance(args.from, None).await.unwrap()
     );
     log::debug!(
         "Receiver's balance before getting payed: {:?}",
         provider.get_balance(args.to, None).await.unwrap()
     );
 
-    let payment_response: TransactionReceipt =
-        SignerMiddleware::send_transaction(&signer_middleware, transaction, None)
-            .await
-            .unwrap()
-            .await
-            .unwrap()
-            .unwrap();
+    let pending_payment_transaction = zk_wallet.transfer(args.to, amount, None).await.unwrap();
+
+    /* Waiting for the payment transaction */
+    let payment_response: TransactionReceipt = pending_payment_transaction.await.unwrap().unwrap();
     log::info!("{:?}", payment_response);
 
     log::debug!(
