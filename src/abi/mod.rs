392ef65bc99963b599234bc33b2c35feb611faa2
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2023-08-28 15:55:59 -0300

    Make ABI loader private so we can use expect calls
    - Why private?
      - This functionality is out of scope for our SDK.
      - It wasn't being used outside this module.
    - I think that it is ok to go with the simplest solution right now. We
      could refactor this to emit an error in the future if necessary.
      - It makes sense to use `expect`/`unwrap` here because the
        `include_str!` macro ensures that the ABI JSON string is present.

--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -3,11 +3,10 @@ use std::str::FromStr;
 
 const L1_DEFAULT_BRIDGE_INTERFACE: &str = include_str!("./IL1Bridge.json");
 
-// FIXME this was taken from
-pub fn load_contract(raw_abi_string: &str) -> Contract {
-    // Note that using `.expect` here is acceptable because we expect the value of
-    // `L1_DEFAULT_BRIDGE_INTERFACE` to be correct. In the future, we should refactor this piece of
-    // code to run in compile time.
+fn load_contract(raw_abi_string: &str) -> Contract {
+    // Note that using `.expect` here is acceptable because this is a private function and we
+    // expect the value of `raw_abi_string` to be correct. In the future, we should refactor this
+    // piece of code to run in compile time.
     #![allow(clippy::unwrap_used, clippy::expect_used)]
     let abi_string = serde_json::Value::from_str(raw_abi_string)
         .expect("Malformed contract abi file")
