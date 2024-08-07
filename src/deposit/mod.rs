77c4a15598e6ffa6dcac56da070e73d12f6cf031
ilitteri <ilitteri@fi.uba.ar>
2024-08-07 10:48:20 -0300

    Refactor deposit module

--- src/deposit/mod.rs
+++ src/deposit/mod.rs
@@ -1,16 +1,6 @@
 // FIXME: Remove this after finishing the implementation.
 #![allow(clippy::unwrap_used)]
 
-use std::sync::Arc;
-
-use ethers::{
-    abi::{Address, Tokenizable},
-    middleware::SignerMiddleware,
-    providers::Middleware,
-    signers::Signer,
-    types::U256,
-};
-
 use crate::{
     contracts::{
         bridgehub::{Bridgehub, L2TransactionRequestDirect, L2TransactionRequestTwoBridgesOuter},
@@ -20,6 +10,17 @@ use crate::{
     types::L1TxOverrides,
     utils, ZKMiddleware,
 };
+use constants::{L1_FEE_ESTIMATION_COEF_DENOMINATOR, L1_FEE_ESTIMATION_COEF_NUMERATOR};
+use ethers::{
+    abi::{Address, Hash, Tokenizable},
+    middleware::SignerMiddleware,
+    providers::Middleware,
+    signers::Signer,
+    types::U256,
+};
+use std::sync::Arc;
+
+mod constants;
 
 pub async fn deposit<M, S, L2Provider>(
     amount: U256,
@@ -27,8 +28,8 @@ pub async fn deposit<M, S, L2Provider>(
     from: Arc<SignerMiddleware<M, S>>,
     to: Address,
     refund_recipient: Address,
-    l2_provider: L2Provider,
-) -> ethers::types::TransactionReceipt
+    l2_provider: &L2Provider,
+) -> Hash
 where
     M: Middleware,
     S: Signer,
@@ -53,23 +54,23 @@ where
         token_to_deposit_is_eth,
         token_to_deposit_is_zk_chain_base_token,
     ) {
-        // Depositing ETH to an ETH based ZKChain.
-        (true, true) => {
-            deposit_eth_to_eth_based_zk_chain(
+        // Depositing base token.
+        (_, true) => {
+            deposit_base_token(
                 amount,
+                token,
                 from,
                 to,
                 refund_recipient,
                 zk_chain_id,
                 l2_provider,
                 bridgehub,
+                token_to_deposit_is_eth,
             )
             .await
         }
         // Depositing ETH to a ZKChain whose base token is not ETH.
         (true, false) => todo!(),
-        // Depositing an ERC20 to a ZKChain whose base token is the same ERC20.
-        (false, true) => todo!(),
         // We are depositing an ERC20 to a ZKChain where:
         // 1. ETH is the base token.
         // 2. An ERC20 different than the deposited is the base token.
@@ -84,6 +85,7 @@ where
                 l2_provider,
                 bridgehub,
                 erc20,
+                false,
             )
             .await
         }
@@ -91,15 +93,17 @@ where
 }
 
 /// Deposit ETH to a ZKChain whose base token is ETH.
-pub async fn deposit_eth_to_eth_based_zk_chain<M, S, L2Provider>(
+pub async fn deposit_base_token<M, S, L2Provider>(
     amount: U256,
+    token: Address,
     from: Arc<SignerMiddleware<M, S>>,
     to: Address,
     refund_recipient: Address,
     zk_chain_id: U256,
-    l2_provider: L2Provider,
+    l2_provider: &L2Provider,
     bridgehub: Bridgehub<SignerMiddleware<M, S>>,
-) -> ethers::types::TransactionReceipt
+    token_is_eth: bool,
+) -> Hash
 where
     M: Middleware,
     S: Signer,
@@ -153,14 +157,40 @@ where
         .factory_deps(vec![])
         .refund_recipient(refund_recipient);
 
+    let base_token_is_eth = token == utils::L2_ETH_TOKEN_ADDRESS;
+    if !base_token_is_eth {
+        let l1_shared_bridge_address: Address = bridgehub.shared_bridge().call().await.unwrap();
+        let erc20 = ERC20::new(token, Arc::<SignerMiddleware<M, S>>::clone(&from));
+
+        let allowance: U256 = erc20
+            .allowance(from.address(), l1_shared_bridge_address)
+            .call()
+            .await
+            .unwrap();
+        // ERC20 approval needs to be done before getting the nonce for the
+        // overrides.
+        if allowance < amount {
+            erc20
+                .approve(l1_shared_bridge_address, amount)
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap()
+                .unwrap();
+        }
+    }
+
+    let value = base_token_is_eth.then_some(l2_costs).unwrap_or_default();
+
     // NOTE: In ETH deposits, the amount of ETH to be deposited into the
     // ETH based chain should be part of the `value` field of the L1 tx
     // which essentially is the call to the `requestL2TransactionDirect`.
     let overrides = L1TxOverrides::default()
         .from(from_addr)
-        .value(l2_costs)
+        .value(value)
         .gas_price(from.get_gas_price().await.unwrap())
-        .gas(estimate_l1_to_l2_gas)
+        .gas(scale_gas_limit(estimate_l1_to_l2_gas))
         .nonce(from.get_transaction_count(from_addr, None).await.unwrap());
 
     deposit_with_l2_transaction_direct_request(bridgehub, request, overrides).await
@@ -170,7 +200,7 @@ pub async fn deposit_with_l2_transaction_direct_request<M, S>(
     bridgehub: Bridgehub<SignerMiddleware<M, S>>,
     request: L2TransactionRequestDirect,
     overrides: L1TxOverrides,
-) -> ethers::types::TransactionReceipt
+) -> Hash
 where
     M: Middleware,
     S: Signer,
@@ -188,6 +218,7 @@ where
         .await
         .unwrap()
         .unwrap()
+        .transaction_hash
 }
 
 /// Deposits ERC20 tokens into a ZKChain where the base token
@@ -198,10 +229,11 @@ pub async fn deposit_non_base_erc20_token<M, S, L2Provider>(
     from: Arc<SignerMiddleware<M, S>>,
     to: Address,
     refund_recipient: Address,
-    l2_provider: L2Provider,
+    l2_provider: &L2Provider,
     bridgehub: Bridgehub<SignerMiddleware<M, S>>,
     erc20: ERC20<SignerMiddleware<M, S>>,
-) -> ethers::types::TransactionReceipt
+    token_to_deposit_is_zk_chain_base_token: bool,
+) -> Hash
 where
     M: Middleware,
     S: Signer,
@@ -228,7 +260,7 @@ where
         l2_gas_per_pubdata_byte_limit,
     )
     .await;
-    let l2_costs = base_cost + operator_tip;
+    let l2_costs = scale_gas_limit(base_cost + operator_tip);
 
     let l1_shared_bridge_address: Address = bridgehub.shared_bridge().call().await.unwrap();
 
@@ -288,11 +320,11 @@ where
     deposit_with_l2_transaction_two_bridges_request(bridgehub, request, overrides).await
 }
 
-async fn deposit_with_l2_transaction_two_bridges_request<M, S>(
+pub async fn deposit_with_l2_transaction_two_bridges_request<M, S>(
     bridgehub: Bridgehub<SignerMiddleware<M, S>>,
     request: L2TransactionRequestTwoBridgesOuter,
     overrides: L1TxOverrides,
-) -> ethers::types::TransactionReceipt
+) -> Hash
 where
     M: Middleware,
     S: Signer,
@@ -309,6 +341,35 @@ where
         .unwrap()
         .await
         .unwrap()
+        .unwrap()
+        .transaction_hash
+}
+
+/// Hash corresponding to the L2 priority operation transaction for finalizing a deposit.
+pub async fn l2_deposit_tx_hash<L1Provider>(
+    l1_deposit_tx_hash: Hash,
+    l1_provider: &L1Provider,
+) -> Hash
+where
+    L1Provider: Middleware,
+{
+    let l1_deposit_tx_receipt = l1_provider
+        .get_transaction_receipt(l1_deposit_tx_hash)
+        .await
+        .unwrap()
+        .unwrap();
+
+    l1_deposit_tx_receipt
+        .logs
+        .into_iter()
+        .find(|event| {
+            event.topics[0]
+                .eq(&ethers::utils::keccak256(
+                    "NewPriorityRequest(uint256,bytes32,uint64,(uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256[4],bytes,bytes,uint256[],bytes,bytes),bytes[])",
+                )
+                .into())
+        })
+        .map(|new_priority_request_event| Hash::from_slice(&new_priority_request_event.data.0[32..64]))
         .unwrap()
 }
 
@@ -335,84 +396,14 @@ where
         .unwrap()
 }
 
-#[cfg(test)]
-mod deposit_tests {
-    use super::deposit;
-    use crate::utils::L2_ETH_TOKEN_ADDRESS;
-    use ethers::{
-        abi::Address,
-        middleware::SignerMiddleware,
-        providers::{Http, Middleware, Provider, ProviderExt},
-        signers::{LocalWallet, Signer},
-        types::U256,
-    };
-    use std::{str::FromStr, sync::Arc};
-
-    #[tokio::test]
-    async fn can_deposit_eth_to_eth_based_zk_chain() {
-        let l1_provider = Provider::<Http>::connect("http://eth-sepolia").await;
-        let l1_chain_id = l1_provider.get_chainid().await.unwrap().as_u64();
-        let from = Arc::new(SignerMiddleware::<Provider<Http>, LocalWallet>::new(
-            l1_provider,
-            LocalWallet::from_str(
-                "0x385c546456b6a603a1cfcaa9ec9494ba4832da08dd6bcf4de9a71e4a01b74924",
-            )
-            .unwrap()
-            .with_chain_id(l1_chain_id),
-        ));
-        let to = from.address();
-        let refund_recipient = from.address();
-        let l2_provider =
-            Provider::<Http>::connect("https://dev.rpc.sepolia.shyft.lambdaclass.com").await;
-        let amount: U256 = ethers::utils::parse_units("0.1", "ether").unwrap().into();
-
-        let receipt = deposit(
-            amount,
-            L2_ETH_TOKEN_ADDRESS,
-            from,
-            to,
-            refund_recipient,
-            l2_provider,
-        )
-        .await;
-
-        println!(
-            "https://sepolia.etherscan.io/tx/{:?}",
-            receipt.transaction_hash
-        );
-    }
-
-    #[tokio::test]
-    async fn deposit_erc20_to_eth_based_zk_chain() {
-        let l1_provider = Provider::<Http>::connect("http://eth-sepolia").await;
-        let l1_chain_id = l1_provider.get_chainid().await.unwrap().as_u64();
-        let from = Arc::new(SignerMiddleware::<Provider<Http>, LocalWallet>::new(
-            l1_provider,
-            LocalWallet::from_str(
-                "0x385c546456b6a603a1cfcaa9ec9494ba4832da08dd6bcf4de9a71e4a01b74924",
-            )
-            .unwrap()
-            .with_chain_id(l1_chain_id),
-        ));
-        let to = from.address();
-        let refund_recipient = from.address();
-        let l2_provider =
-            Provider::<Http>::connect("https://dev.rpc.sepolia.shyft.lambdaclass.com").await;
-        let amount: U256 = ethers::utils::parse_units("0.1", "ether").unwrap().into();
-
-        let receipt = deposit(
-            amount,
-            Address::from_str("0x7209EaD3dfe1c3a517B6b730C00bea1E7f319260").unwrap(),
-            from,
-            to,
-            refund_recipient,
-            l2_provider,
-        )
-        .await;
-
-        println!(
-            "https://sepolia.etherscan.io/tx/{:?}",
-            receipt.transaction_hash
-        );
-    }
+/// Scales the provided gas limit using a coefficient to ensure acceptance of L1->L2 transactions.
+///
+/// This function adjusts the gas limit by multiplying it with a coefficient calculated from the
+/// `L1_FEE_ESTIMATION_COEF_NUMERATOR` and `L1_FEE_ESTIMATION_COEF_DENOMINATOR` constants.
+pub fn scale_gas_limit(gas_limit: U256) -> U256 {
+    gas_limit
+        .checked_mul(U256::from(L1_FEE_ESTIMATION_COEF_NUMERATOR))
+        .unwrap()
+        .checked_div(U256::from(L1_FEE_ESTIMATION_COEF_DENOMINATOR))
+        .unwrap()
 }
