c7d365970e475778f18be96bb10d881e468c009c
ilitteri <ilitteri@fi.uba.ar>
2024-08-13 00:08:21 -0300

    Update test helpers

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,30 +1,36 @@
 use std::{str::FromStr, sync::Arc};
 
 use ethers::{
-    abi::{Abi, Address, Hash},
+    abi::{Address, Hash},
     prelude::SignerMiddleware,
     providers::{Http, Middleware, Provider, ProviderExt},
     signers::{LocalWallet, Signer},
-    types::{Bytes, U256, U64},
+    types::{U256, U64},
 };
-use serde::Deserialize;
 use zksync_ethers_rs::{
     utils::{L1_ETH_TOKEN_ADDRESS, L2_ETH_TOKEN_ADDRESS},
     ZKMiddleware,
 };
 
-#[derive(Deserialize)]
-pub struct CompiledContract {
-    pub abi: Abi,
-    pub bin: Bytes,
+pub const L2_RPC_URL: &str = "http://127.0.0.1:3050";
+pub const L2_EXPLORER_URL: &str = "http://127.0.0.1:3010";
+pub const L1_EXPLORER_URL: &str = "";
+pub const L1_RPC_URL: &str = "http://127.0.0.1:8545";
+
+pub fn l1_explorer_url() -> String {
+    std::env::var("L1_EXPLORER_URL").unwrap_or(L1_EXPLORER_URL.to_owned())
+}
+
+pub fn l2_explorer_url() -> String {
+    std::env::var("L2_EXPLORER_URL").unwrap_or(L2_EXPLORER_URL.to_owned())
 }
 
 pub async fn l1_provider() -> Provider<Http> {
-    Provider::<Http>::connect("http://eth-sepolia").await
+    Provider::<Http>::connect(&std::env::var("L1_RPC_URL").unwrap_or(L1_RPC_URL.to_owned())).await
 }
 
 pub async fn l2_provider() -> Provider<Http> {
-    Provider::<Http>::connect("https://k8s.rpc.sepolia.shyft.lambdaclass.com").await
+    Provider::<Http>::connect(&std::env::var("L2_RPC_URL").unwrap_or(L2_RPC_URL.to_owned())).await
 }
 
 pub async fn signer(
@@ -33,9 +39,11 @@ pub async fn signer(
     let chain_id = provider.get_chainid().await.unwrap().as_u64();
     Arc::new(SignerMiddleware::<Provider<Http>, LocalWallet>::new(
         provider,
-        LocalWallet::from_str("0x385c546456b6a603a1cfcaa9ec9494ba4832da08dd6bcf4de9a71e4a01b74924")
-            .unwrap()
-            .with_chain_id(chain_id),
+        LocalWallet::from_str(&std::env::var("PRIVATE_KEY").unwrap_or(
+            "0x385c546456b6a603a1cfcaa9ec9494ba4832da08dd6bcf4de9a71e4a01b74924".to_owned(),
+        ))
+        .unwrap()
+        .with_chain_id(chain_id),
     ))
 }
 
