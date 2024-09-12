2013167c0e019127a9802912ddfaae17eb7e2769
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-09-12 17:48:02 -0300

    Use append option for correct rlp length in paymaster params

--- src/eip712/meta.rs
+++ src/eip712/meta.rs
@@ -75,8 +75,6 @@ impl Encodable for Eip712Meta {
         // 14
         rlp_append_option(stream, self.custom_signature.clone().map(|v| v.to_vec()));
         // 15
-        if let Some(paymaster_params) = self.paymaster_params.clone() {
-            paymaster_params.rlp_append(stream);
-        }
+        rlp_append_option(stream, self.paymaster_params.clone());
     }
 }
